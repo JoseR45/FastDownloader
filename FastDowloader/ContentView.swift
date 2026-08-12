afa3887f20f8ce1114e7e62911d402502bdf4fe1
José Ramón <jose.1999.rfg@gmail.com>
2026-08-12 17:36:45 -0400

    Initial Commit

---
+++ FastDowloader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FastDowloader
+//
+//  Created by Jose Fidalgo on 12-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastDowloader/FastDowloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastDowloaderApp.swift
+//  FastDowloader
+//
+//  Created by Jose Fidalgo on 12-08-26.
+//
+
+import SwiftUI
+
+@main
+struct FastDowloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
